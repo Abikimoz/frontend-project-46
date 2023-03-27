1bfe11c6f244ea66581da853c6f15784abbf52ee
abikimoz <i.m.anisimov25@gmail.com>
2023-03-27 23:37:34 +0300

    шаг 2 выполнен

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,8 +3,11 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .name('gendiff')
-  .description('CLI to some JavaScript string utilities')
-  .version('0.0.1');
+  .name('gendiff', '<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1> <filepath2>')
+  .version('0.0.1')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
