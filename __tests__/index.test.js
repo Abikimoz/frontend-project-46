9506a7c4e256006938efd6aeabd86a58c123941f
abikimoz <i.m.anisimov25@gmail.com>
2023-04-25 22:44:56 +0300

    добавил yml тесты

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,14 @@ import { test, expect } from '@jest/globals';
 import result from '../__fixtures__/result.js';
 import showDiff from '../src/index.js';
 
-test('gendiff', () => {
+test('json', () => {
   expect(showDiff('file1.json', 'file2.json')).toBe(result);
-//   expect(1).toBe(1);
+});
+
+test('yaml', () => {
+  expect(showDiff('file1.yaml', 'file2.yaml')).toBe(result);
+});
+
+test('yml', () => {
+  expect(showDiff('file1.yml', 'file2.yml')).toBe(result);
 });
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,6 +11,9 @@ program
   .version('0.0.1')
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format <type>', 'output format')
-  .action(showDiff);
+  .action((filepath1, filepath2) => {
+    const result = showDiff(filepath1, filepath2);
+    console.log(result);
+  });
 
 program.parse();
